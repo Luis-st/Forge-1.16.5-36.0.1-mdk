005cd9558756be3c87a1f1fd46bea1c00e083dcf
BuildTools <unconfigured@null.spigotmc.org>
2021-01-21 21:54:45 +0100

    Edit thunderbolt ench add !world.isRemote

--- forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/events/entity/OnEntityAttack.java
+++ forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/events/entity/OnEntityAttack.java
@@ -12,9 +12,9 @@ import net.minecraft.entity.effect.LightningBoltEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -68,23 +68,17 @@ public class OnEntityAttack {
 				
 				if (enchThunderbolt > 0) {
 					
-					MinecraftServer server = player.getEntityWorld().getServer();
-					
 					LightningBoltEntity lightning = new LightningBoltEntity(EntityType.LIGHTNING_BOLT, world);
 					lightning.setLocationAndAngles(target.getPosX(), target.getPosY(), target.getPosZ(), world.rand.nextFloat() * 360, 0);
 					world.addEntity(lightning);
 					
-//					((ServerWorld) world).func_241113_a_(0, world.rand.nextInt(1000), true, true);
-					
-					if (server != null) {
+					if (!world.isRemote) {
 						
-						server.getWorlds().forEach(w -> {
-							
-							w.func_241113_a_(0, world.rand.nextInt(1000), true, true);
-							
-						});
+						((ServerWorld) world).func_241113_a_(0, world.rand.nextInt(1000), true, true);
 						
 					}
+					
+					player.addPotionEffect(new EffectInstance(Effects.RESISTANCE, 30, 4, false, false));
 					
 				}
 				
