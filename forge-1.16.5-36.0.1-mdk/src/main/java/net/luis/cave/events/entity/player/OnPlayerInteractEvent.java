a77f90f1bc65da16976517a85c1e0663ed6be5ca
BuildTools <unconfigured@null.spigotmc.org>
2021-01-23 15:54:59 +0100

    Add Bonemeal action to replenish

--- forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/events/entity/player/OnPlayerInteractEvent.java
+++ forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/events/entity/player/OnPlayerInteractEvent.java
@@ -5,10 +5,16 @@ import java.lang.reflect.Method;
 import java.util.UUID;
 
 import net.luis.cave.Cave;
+import net.luis.cave.lib.ItemManager;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CropsBlock;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.ZombieVillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,7 +27,7 @@ public class OnPlayerInteractEvent {
 	private static final Method startConverting = ObfuscationReflectionHelper.findMethod(ZombieVillagerEntity.class, "func_191991_a", UUID.class, int.class);
 	
 	@SubscribeEvent
-	public static void PlayerInteract(PlayerInteractEvent.EntityInteractSpecific event) {
+	public static void EntityInteractSpecific(PlayerInteractEvent.EntityInteractSpecific event) {
 		
 		PlayerEntity player = event.getPlayer();
 		LivingEntity target = (LivingEntity) event.getTarget();
@@ -60,6 +66,35 @@ public class OnPlayerInteractEvent {
 					}
 					
 				}
+				
+			}
+			
+		}
+
+	}
+	
+	@SubscribeEvent
+	public static void PlayerInteract(PlayerInteractEvent.RightClickBlock event) {
+		
+		PlayerEntity player = event.getPlayer();
+		BlockPos pos = event.getPos();
+		World world = event.getWorld();
+		BlockState state = world.getBlockState(pos);
+		
+		if (state.getBlock() instanceof CropsBlock) {
+			
+			CropsBlock cropsBlock = (CropsBlock) state.getBlock();
+			int age = state.get(cropsBlock.getAgeProperty());
+			
+			if (!cropsBlock.isMaxAge(state)) {
+			
+				int newAge = MathHelper.nextInt(player.getRNG(), age + 1, cropsBlock.getMaxAge());
+				world.setBlockState(pos, state.with(cropsBlock.getAgeProperty(), newAge), 3);
+				ItemManager.damageItem(player.getHeldItemMainhand(), newAge > 5 ? 5 : newAge, player, e -> {
+					
+					e.sendBreakAnimation(EquipmentSlotType.MAINHAND);
+					
+				}, false);
 				
 			}
 			
--- forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/events/world/OnReplenish.java
+++ forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/events/world/OnReplenish.java
@@ -1,5 +1,7 @@
 package net.luis.cave.events.world;
 
+import java.util.List;
+
 import net.luis.cave.Cave;
 import net.luis.cave.init.CaveEnchantment;
 import net.luis.cave.lib.ItemManager;
@@ -8,13 +10,18 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.CropsBlock;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 @Mod.EventBusSubscriber(modid=Cave.Mod_Id, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class OnReplenish {
@@ -27,6 +34,8 @@ public class OnReplenish {
 		PlayerEntity player = event.getPlayer();
 		BlockState state = event.getState();
 		int enchReplenish = EnchantmentHelper.getEnchantmentLevel(CaveEnchantment.REPLENISH.get(), player.getHeldItemMainhand());
+		int enchTelekinesis = EnchantmentHelper.getEnchantmentLevel(CaveEnchantment.TELEKINESIS.get(), player.getHeldItemMainhand());
+		int enchFortune = EnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, player.getHeldItemMainhand());
 		
 		if (player instanceof PlayerEntity) {
 		
@@ -40,7 +49,52 @@ public class OnReplenish {
 						
 						if (cropsBlock.isMaxAge(world.getBlockState(pos))) {
 							
-							Block.spawnDrops(state, world, pos);
+							if (enchTelekinesis > 0) {
+								
+								List<ItemStack> drops = Block.getDrops(state, (ServerWorld) world, pos, null);
+								
+								drops.forEach(item -> {
+									
+									ItemHandlerHelper.giveItemToPlayer(player, item);
+									
+								});
+								
+							} else {
+								
+								Block.spawnDrops(state, world, pos);
+								
+							}
+							
+							event.setCanceled(true);
+							
+							world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
+							
+							if (cropsBlock == Blocks.WHEAT) {
+								
+								world.setBlockState(pos, state.with(cropsBlock.getAgeProperty(), 
+										enchFortune == 0 ? 0 : MathHelper.nextInt(world.rand, 0 + enchFortune, cropsBlock.getMaxAge() - 1)), 3);
+								
+							} else if (cropsBlock == Blocks.POTATOES) {
+								
+								world.setBlockState(pos, state.with(cropsBlock.getAgeProperty(), 
+										enchFortune == 0 ? 0 : MathHelper.nextInt(world.rand, 0 + enchFortune, cropsBlock.getMaxAge() - 1)), 3);
+								
+							} else if (cropsBlock == Blocks.CARROTS) {
+								
+								world.setBlockState(pos, state.with(cropsBlock.getAgeProperty(), 
+										enchFortune == 0 ? 0 : MathHelper.nextInt(world.rand, 0 + enchFortune, cropsBlock.getMaxAge() - 1)), 3);
+								
+							} else if (cropsBlock == Blocks.BEETROOTS) {
+								
+								world.setBlockState(pos, state.with(cropsBlock.getAgeProperty(), 
+										enchFortune == 0 ? 0 : MathHelper.nextInt(world.rand, 0 + enchFortune, cropsBlock.getMaxAge() - 1)), 3);
+								
+							}
+							
+							player.getHeldItemMainhand().damageItem(1, player, e -> e.sendBreakAnimation(EquipmentSlotType.MAINHAND));
+							
+						} else {
+							
 							event.setCanceled(true);
 							
 							world.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
@@ -63,7 +117,7 @@ public class OnReplenish {
 								
 							}
 							
-							ItemManager.unbreaking(player, player.getHeldItemMainhand(), EquipmentSlotType.MAINHAND);
+							player.getHeldItemMainhand().damageItem(1, player, e -> e.sendBreakAnimation(EquipmentSlotType.MAINHAND));
 							
 						}
 						
