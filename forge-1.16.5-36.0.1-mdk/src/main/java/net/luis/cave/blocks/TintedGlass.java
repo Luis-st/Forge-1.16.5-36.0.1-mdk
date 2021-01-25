f6509652bd58630605f5e4978e0030600b43b6ee
BuildTools <unconfigured@null.spigotmc.org>
2021-01-25 13:35:49 +0100

    Add Tinted Glass

---
+++ forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/blocks/TintedGlass.java
@@ -0,0 +1,43 @@
+package net.luis.cave.blocks;
+
+import net.minecraft.block.AbstractGlassBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+
+public class TintedGlass extends AbstractGlassBlock {
+
+	public TintedGlass() {
+		
+		super(Block.Properties.create(Material.GLASS)
+				.zeroHardnessAndResistance()
+				.sound(SoundType.GLASS)
+				.notSolid());
+
+	}
+	
+	@Override
+	public float getAmbientOcclusionLightValue(BlockState state, IBlockReader worldIn, BlockPos pos) {
+		
+		return 0.0f;
+		
+	}
+	
+	@Override
+	public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+		
+		return false;
+		
+	}
+	
+	@Override
+	public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+		
+		return 0;
+		
+	}
+
+}
