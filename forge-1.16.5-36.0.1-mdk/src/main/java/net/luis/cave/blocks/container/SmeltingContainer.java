b55bad37a4e156a1cbc7979697809e027e3a4589
BuildTools <unconfigured@null.spigotmc.org>
2021-02-02 10:17:10 +0100

    Add custom Container Type

--- forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/blocks/container/SmeltingContainer.java
+++ forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/blocks/container/SmeltingContainer.java
@@ -1,5 +1,6 @@
 package net.luis.cave.blocks.container;
 
+import net.luis.cave.init.CaveContainer;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.AbstractFurnaceContainer;
@@ -10,7 +11,7 @@ public class SmeltingContainer extends AbstractFurnaceContainer {
 
 	public SmeltingContainer(int id, PlayerInventory playerInventory, IInventory furnaceInventoryIn, IIntArray p_i241922_7_) {
 		
-		super(/**CaveContainer.SMELTING_CONTAINER.get()**/null, null, RecipeBookCategory.FURNACE, id, playerInventory, furnaceInventoryIn, p_i241922_7_);
+		super(CaveContainer.SMELTING_CONTAINER.get(), null, RecipeBookCategory.FURNACE, id, playerInventory, furnaceInventoryIn, p_i241922_7_);
 		
 	}
 
