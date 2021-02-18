a135b83d8cfd5835088d7759c23063f43263c144
BuildTools <unconfigured@null.spigotmc.org>
2021-02-18 12:17:22 +0100

    add damage star new function

--- forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/common/item/star/DamageStar.java
+++ forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/common/item/star/DamageStar.java
@@ -1,23 +1,16 @@
 package net.luis.cave.common.item.star;
 
-import java.util.List;
-
 import net.luis.cave.Cave;
 import net.luis.cave.api.item.api.Star;
 import net.luis.cave.common.enums.StarType;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.world.World;
 
 public class DamageStar extends Star {
-	
-	private final String tag = this.getTagName();
-	private final String maxTag = "max";
 
 	public DamageStar() {
 		
@@ -28,38 +21,8 @@ public class DamageStar extends Star {
 	@Override
 	public void starTick(ItemStack stack, World world, PlayerEntity player) {
 		
-		if (stack.getOrCreateChildTag(this.getTagName()).getDouble(this.getTagName()) >= stack.getOrCreateChildTag(this.getMaxTag()).getInt(this.getMaxTag())
-				) {
-			
-			stack.getOrCreateChildTag(this.getTagName()).putDouble(this.getTagName(), 0);
-			stack.getOrCreateChildTag(this.getMaxTag()).putInt(this.getMaxTag(), stack.getOrCreateChildTag(this.getMaxTag()).getInt(this.getMaxTag()) + 1);
-			
-		}
-		
-	}
-	
-	@Override
-	public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip, ITooltipFlag flag) {
-		
-		int tagValue = (int) stack.getOrCreateChildTag(this.getTagName()).getDouble(this.getTagName());
-		int maxTag = stack.getOrCreateChildTag(this.getMaxTag()).getInt(this.getMaxTag());
-		tooltip.add(new StringTextComponent("Maximum Bonus Damage: ")
-				.mergeStyle(TextFormatting.BLUE).append(new StringTextComponent("" + maxTag)));
-		tooltip.add(new StringTextComponent("Bonus Damage: ")
-					.mergeStyle(TextFormatting.BLUE).append(new StringTextComponent("" + tagValue)));
-		
-	}
-
-	public String getMaxTag() {
-		
-		return maxTag;
-		
-	}
-	
-	@Override
-	public String getTagName() {
-		
-		return this.tag;
+		player.addPotionEffect(new EffectInstance(new EffectInstance(Effects.STRENGTH, 10, 1, true, false)));
+		player.addPotionEffect(new EffectInstance(new EffectInstance(Effects.RESISTANCE, 10, 0, true, false)));
 		
 	}
 
--- forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/events/entity/living/OnLivingDamageEvent.java
+++ forge-1.16.5-36.0.1-mdk/src/main/java/net/luis/cave/events/entity/living/OnLivingDamageEvent.java
@@ -2,14 +2,10 @@ package net.luis.cave.events.entity.living;
 
 import net.luis.cave.Cave;
 import net.luis.cave.api.entity.EntityManager;
-import net.luis.cave.api.entity.player.PlayerManager;
-import net.luis.cave.common.item.star.DamageStar;
 import net.luis.cave.init.ModEnchantment;
-import net.luis.cave.init.items.ModItems;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -36,6 +32,7 @@ public class OnLivingDamageEvent {
 				if (enchEnderSlayer > 0) {
 					
 					newAmount += (enchEnderSlayer * 2.5f) + enchEnderSlayer;
+					event.setAmount(newAmount);
 					
 				}
 				
@@ -46,48 +43,9 @@ public class OnLivingDamageEvent {
 				if (enchImpaling > 0) {
 					
 					newAmount += (enchImpaling * 2.5f) + enchImpaling;
+					event.setAmount(newAmount);
 					
 				}
-				
-			}
-			
-			if (PlayerManager.getItem(player, ModItems.DAMAGE_STAR.get()).getItem() != ItemStack.EMPTY.getItem()) {
-				
-				ItemStack stack = PlayerManager.getItem(player, ModItems.DAMAGE_STAR.get());
-				
-				if (stack.getItem() instanceof DamageStar) {
-					
-					DamageStar star = (DamageStar) stack.getItem();
-					final String tag = star.getTagName();
-					final String max = star.getMaxTag();
-					
-					newAmount += stack.getOrCreateChildTag(tag).getDouble(tag);
-					
-					if (stack.getOrCreateChildTag(max).getInt(max) > 0) {
-						
-
-						
-					}
-					
-					if (stack.getOrCreateChildTag(tag).getDouble(tag) < stack.getOrCreateChildTag(max).getInt(max)) {
-						
-						stack.getOrCreateChildTag(tag).putDouble(tag, stack.getOrCreateChildTag(tag).getDouble(tag) + 1);
-						
-					}
-					
-				}
-				
-			}
-			
-			if (target instanceof PlayerEntity) {
-				
-				boolean flag = (newAmount - amount) >= 10;
-				float playerAmount = flag ? amount + 10 : newAmount;
-				event.setAmount(playerAmount);
-				
-			} else {
-				
-				event.setAmount(newAmount);
 				
 			}
 			
